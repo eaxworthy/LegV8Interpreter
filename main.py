dec131647c5ab407bf8a51fd670cbe91db0e0ca5
eaxworthy <lizzieaxworthy@gmail.com>
2019-03-18 16:14:46 -0700

    Created state file and funcs file

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 import numpy as np
+import funcs as f
+import state as s
 
-registers = [0x00000000] * 32
+
+functions = {
+    'ADD': f.add,
+    'ADDI': f.addi,
+    'ADDS': f.adds,
+    'ADDIS': f.addis,
+    'AND': f.aand, #needs to be spelled like this because 'and' is reserved
+    'ANDI': f.andi,
+    'ANDS': f.ands,
+    'ANDIS': f.andis,
+    'B': f.b,
+    'B.cond': f.bcond,
+    'BL': f.bl,
+    'BR': f.br,
+    'CBZ': f.cbz,
+    'CBNZ': f.cbnz,
+    'EOR': f.eor,
+    'EORI': f.eori,
+    'LDUR': f.ldur,
+    'LDURB': f.ldurb,
+    'LDURH': f.ldurh,
+    'LDURSW': f.ldursw,
+    'LDXR': f.ldxr,
+    'LSL': f.lsl,
+    'LSR': f.lsr,
+    'ORR': f.orr,
+    'ORRI': f.orri,
+    'SUB': f.sub,
+    'SUBI': f.subi,
+    'SUBS': f.subs,
+    'SUBIS': f.subis,
+    'STUR': f.stur,
+    'STURB': f.sturb,
+    'STURH': f.sturh,
+    'STURW': f.sturw,
+    'STXR': f.stxr,
+}
+
+#
+#x = input("Enter Function: ")
+#functions[x](['Hello', 3, 'GoodBye'])
